d84404ff980914ce4a3643bde8a2ad9b0563216c
Wampastomp99 <michaelheartbound99@hotmail.com>
2021-03-07 17:48:53 -0700

    added java

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,23 @@ var today = moment().format('DD' + "/" + 'MM' + '/' + 'YYYY');
 var APIKey = "&units=metric&APPID=f90ce9aeaf95d88d4e7a73c1f7196654";
 var url =  "https://api.openweathermap.org/data/2.5/weather?q=";
 var citiesArray = JSON.parse(localStorage.getItem("Saved City")) || [];
+
+$(document).ready(function(){
+    var userInput = citiesArray[citiesArray.length - 1];
+    curentWeather(userInput);
+    forecastDate(userInput);
+    lastSearch ();
+});
+
+function currentWeather(userInput) {
+    mainIcon.empty();
+    var queryUrl = url + userInput + APIKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        var cityInfo = response.name;
+        var country = response.sys.country;
+        var temp
+    })
+}
